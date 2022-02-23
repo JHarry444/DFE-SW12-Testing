99d768413dffb0baf36693d207300b5696de4d56
jordan <jharrison1@qa.com>
2022-02-23 12:27:35 +0000

    init commit

---
+++ src/main/java/com/qa/testing/Calculator.java
@@ -0,0 +1,20 @@
+package com.qa.testing;
+
+public class Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
---
+++ src/test/java/com/qa/testing/CalcTest.java
@@ -0,0 +1,26 @@
+package com.qa.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	private Calculator calc = new Calculator();
+
+	@Test
+	public void testSum() {
+		int expected = 2;
+		int actual = this.calc.sum(1, 1);
+
+		assertEquals(expected, actual); // asserts that expected equals value
+	}
+
+	@Test
+	public void testSubtract() {
+		int expected = 2;
+		int actual = this.calc.subtract(4, 2);
+
+		assertEquals(expected, actual);
+	}
+}
